56e9e054fdf7ba20e92496077cce96c444e518ee
25300439 <25300439@K216W10>
2026-09-09 09:16:11 +0500

    Добавьте файлы проекта.

---
+++ lesson1/MMath.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace I207
+{
+    public class MMath:I207.Math
+    {
+        void test1()
+        {
+            sum(5, 6);
+        }
+    }
+}
---
+++ lesson2/HMath.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+namespace I207
+{
+    internal class HMath : I207.MMath
+    {
+        void test()
+        {
+            sum(3, 7);
+        }
+    }
+}
